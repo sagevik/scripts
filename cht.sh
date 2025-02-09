de976c45916e18eaa5071b5a88a88527eb01bf7f
sagevik <rsagevik@gmail.com>
2025-02-09 21:06:10 +0100

    make output operate same way on language and command

--- cht.sh
+++ cht.sh
@@ -5,11 +5,13 @@ if [[ -z $selected ]]; then
     exit 0
 fi
 
-read -p "Enter Query: " query
+read -p "[ $selected ] Enter Query: " query
 
 if grep -qs "$selected" ~/.config/tmux/tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
+    #tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query | less -R"
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
+    #tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
 fi
