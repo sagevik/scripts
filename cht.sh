113b3b1e8f3527f663ae52ed58a24c308f73ab4c
sagevik <rsagevik@gmail.com>
2025-04-13 12:34:03 +0200

    check for tmux and change fzf prompt

--- cht.sh
+++ cht.sh
@@ -1,17 +1,25 @@
 #! /usr/bin/env bash
 
-selected=`cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | fzf --reverse`
+if [[ -z "$TMUX" ]]; then
+    notify-send "cht.sh" "Will only run in a tmux session"
+    exit 0
+fi
+
+FZF_OPTS="--tmux center,50%,25%,border-native --layout=reverse"
+selected=`cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | fzf $FZF_OPTS`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-read -p "[ $selected ] Enter Query: " query
+FZF_OPTS="--tmux center,50%,5%,border-native --layout=reverse"
+query=$(echo | fzf $FZF_OPTS --prompt "$selected -> Enter Query: " --print-query | tail -n1)
+if [[ -z $query ]]; then
+    exit 0
+fi
 
 if grep -qs "$selected" ~/.config/tmux/tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    #tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query | less -R"
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    query=$(echo $query | tr ' ' '+')
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & sleep infinity"
 else
-    #tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
-    tmux neww bash -c "curl -s cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query & sleep infinity"
 fi
