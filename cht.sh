f829a2dd085a9a3d727295efc1d1ad88901ea241
sagevik <rsagevik@gmail.com>
2025-04-18 12:12:12 +0200

    adjust size and disable fzf preview

--- cht.sh
+++ cht.sh
@@ -5,13 +5,13 @@ if [[ -z "$TMUX" ]]; then
     exit 0
 fi
 
-FZF_OPTS="--tmux center,50%,25%,border-native --layout=reverse --no-preview"
+FZF_OPTS="--tmux center,50%,40%,border-native --layout=reverse --no-preview"
 selected=`cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | fzf $FZF_OPTS`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-FZF_OPTS="--tmux center,50%,5%,border-native --layout=reverse"
+FZF_OPTS="--tmux center,50%,5%,border-native --layout=reverse --no-preview"
 query=$(echo | fzf $FZF_OPTS --prompt "$selected -> Enter Query: " --print-query | tail -n1)
 if [[ -z $query ]]; then
     exit 0
