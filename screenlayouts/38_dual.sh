ad831368bd327d43d9e41340585cd6d6d54fdf24
sagevik <rsagevik@gmail.com>
2025-05-31 08:01:34 +0200

    move screenlayouts

---
+++ screenlayouts/38_dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1-1 --mode 1920x1200 --pos 0x400 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-1-1 --off --output DP-1-1-2 --mode 3840x1600 --pos 1920x0 --rotate normal --output DP-1-1-3 --off
